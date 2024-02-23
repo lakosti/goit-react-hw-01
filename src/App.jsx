3865c4211e075798d8da2f98498cc94e6cabe87e
Yaroslava <yaroslava.hovenko@gmail.com>
2024-02-23 22:20:15 +0200

    1 task render

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import Profile from "./components/Profile";
+
+import userData from "./userData.json";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+const Profile = ({ name, tag, location, image, stats: { followers, views, likes } }) => {
+  return (
+    <div>
+      <div>
+        <div>
+          <img src={image} alt="User avatar" />
+          <p>{name}</p>
+          <p>@{tag}</p>
+          <p>{location}</p>
+        </div>
+
+        <ul>
+          <li>
+            <span>Followers</span>
+            <span>{followers}</span>
+          </li>
+          <li>
+            <span>Views</span>
+            <span>{views}</span>
+          </li>
+          <li>
+            <span>Likes</span>
+            <span>{likes}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
